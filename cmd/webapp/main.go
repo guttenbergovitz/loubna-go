d993f2461c9c6bc65dc1ebe9ace056c674383307
(ツ) <guttenbergovitz@gmail.com>
2024-09-28 19:50:46 +0200

    feat: add wrapper in templ

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/guttenbergovitz/loubna-go/web/templates"
 	"net/http"
-
-	"github.com/a-h/templ"
 )
 
 func main() {
-	component := templates.Hello("Loubna Photo")
+	fs := http.FileServer(http.Dir("./web/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.Handle("/", templ.Handler(component))
+	component := templates.Hello("Loubna Photo")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := templates.Layout(component).Render(context.Background(), w)
+		if err != nil {
+			http.Error(w, "Failed to render page", http.StatusInternalServerError)
+		}
+	})
 
 	fmt.Println("Listening on :3000")
 	err := http.ListenAndServe(":3000", nil)
